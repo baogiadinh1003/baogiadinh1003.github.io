662fbddd056e48f1602382ac9453fb08f6666834
baogiadinh1003 <baodpgpd03125@gmail.com>
2021-12-11 22:53:52 +0700

    done 40%

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
-var dashboardAction = true;
-var pitchAction = false;
-var accountListAccount = false;
-var dashboardHtml = './dashboard.html';
 var dataRenter;
 var dataOwner;
 var dataPitch;
-
+var dataAvgTotalPitchPerOwner;
 $(document).ready(function() {
     // menu
+    loadAvgTotalPitchPerOwner();
     loadAccountData();
     if ($("#selectPitch").val() == 'city') {
         loadPitchDataCity();
@@ -27,6 +24,33 @@ $(document).ready(function() {
             $("#chartPitchAvg").removeClass('d-none');
             loadPitchDataAvgPrice();
         }
+    });
+
+    $("#dashboard").click(function(e) {
+        $(".dashboard-container").removeClass('d-none');
+        $(".dashboard-container").addClass('d-flex');
+        $(".account-container").addClass('d-none');
+        $(".account-container").removeClass('d-flex');
+        $(".pitch-container").addClass('d-none');
+        $(".pitch-container").removeClass('d-flex');
+    });
+
+    $("#pitch").click(function(e) {
+        $(".pitch-container").removeClass('d-none');
+        $(".pitch-container").addClass('d-flex');
+        $(".account-container").addClass('d-none');
+        $(".account-container").removeClass('d-flex');
+        $(".dashboard-container").addClass('d-none');
+        $(".dashboard-container").removeClass('d-flex');
+    });
+
+    $("#accountList").click(function(e) {
+        $(".account-container").removeClass('d-none');
+        $(".account-container").addClass('d-flex');
+        $(".dashboard-container").addClass('d-none');
+        $(".dashboard-container").removeClass('d-flex');
+        $(".pitch-container").addClass('d-none');
+        $(".pitch-container").removeClass('d-flex');
     });
 });
 
@@ -58,7 +82,7 @@ async function loadAccountData() {
         data: {
             labels: ['Tổng', 'Người thuê', 'Người cho thuê'],
             datasets: [{
-                label: 'Quantily',
+                label: 'Số lượng',
                 data: [Number(countRenter) + Number(countOwner), countRenter, countOwner],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
@@ -131,7 +155,7 @@ async function loadPitchDataCity() {
         data: {
             labels: addressFilter,
             datasets: [{
-                label: 'Quantily',
+                label: 'Số lượng',
                 data: countPitch,
                 backgroundColor: [
                     'rgba(79, 192, 192, 0.2)',
@@ -208,7 +232,7 @@ async function loadPitchDataAvgPrice() {
         data: {
             labels: ['Sân trên giá trung bình', 'Sân dưới giá trung bình', 'Sân giá trung bình'],
             datasets: [{
-                label: 'Quantily',
+                label: 'Số lượng',
                 data: [overAvg, underAvg, isAvg],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
@@ -234,6 +258,88 @@ async function loadPitchDataAvgPrice() {
                 title: {
                     display: true,
                     text: `Thống kê sân theo giá trung bình (${avgPrice})`,
+                    position: 'bottom'
+                }
+            }
+
+        }
+    });
+}
+
+async function loadAvgTotalPitchPerOwner() {
+    await $.ajax({
+        method: "POST",
+        url: "https://we-sports-sv.herokuapp.com/v1/pitch/list",
+        contentType: "application/json",
+        dataType: 'json',
+        success: function(result) {
+            var dum = result;
+            dataAvgTotalPitchPerOwner = dum.data;
+        }
+    })
+
+    var ownersId = [];
+    var ownersName = ['Trung bình'];
+    for (let index = 0; index < dataAvgTotalPitchPerOwner.length; index++) {
+        var element = dataAvgTotalPitchPerOwner[index];
+        if (ownersId.indexOf(element.pitchOwner._id) !== -1) {
+            continue;
+        }
+        ownersId.push(element.pitchOwner._id)
+        ownersName.push(element.pitchOwner.ownerName)
+    }
+
+    var countPitch = [0];
+    var avg = 0;
+    for (let index = 0; index < ownersId.length; index++) {
+        const element = ownersId[index];
+        data = { pitchOwner: element };
+        await $.ajax({
+            method: "POST",
+            url: "https://we-sports-sv.herokuapp.com/v1/pitch/listbyowner",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            success: function(result) {
+                avg += result.data.length;
+                countPitch.push(result.data.length);
+            }
+        })
+    }
+
+    avg = avg / ownersId.length;
+    countPitch[0] = avg;
+    var max = Math.max.apply(Math, countPitch);
+    var min = Math.min.apply(Math, countPitch);
+    var indexOfMax = countPitch.indexOf(max);
+    var indexOfMin = countPitch.indexOf(min);
+    var ctx = document.getElementById('chartAvgTotalPitchPerOwner').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: [ownersName[0], "Người nhiều sân nhất: " + ownersName[indexOfMax], "Người ít sân nhất: " + ownersName[indexOfMin]],
+            datasets: [{
+                label: 'Số lượng sân',
+                data: [countPitch[0], countPitch[indexOfMax], countPitch[indexOfMin]],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'bottom',
+                },
+                title: {
+                    display: true,
+                    text: 'Thống kê sân',
                     position: 'bottom'
                 }
             }
